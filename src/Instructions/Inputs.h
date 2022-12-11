1db7a7a1f1b73896aa2b5c77eb94cfed9367b4e3
Program <67320703+Program132@users.noreply.github.com>
2022-12-11 17:05:17 +0100

    Added an input instruction function.

---
+++ src/Instructions/Inputs.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "../Essentials/Types.h"
+#include "../Essentials/Utils/Data.h"
+#include "../Essentials/Utils/ErrorsCodeManagement.h"
+
+namespace FPL::Instruction {
+    std::string returnInputFinalValue();
+}
