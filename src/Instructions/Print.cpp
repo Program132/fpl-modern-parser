c179dbad6c2d570de1a257ccd92b49083c3daf4e
Program <67320703+Program132@users.noreply.github.com>
2022-12-11 17:05:39 +0100

    Prints function.

---
+++ src/Instructions/Print.cpp
@@ -0,0 +1,31 @@
+#include "Print.h"
+
+namespace FPL::Instruction {
+    void operatorCondReturnFalseTrue_INT(std::string operatorCond, int a, int b) {
+        if (operatorCond == "=") {
+            if (a == b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == "<") {
+            if (a < b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == ">") {
+            if (a > b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == "<=") {
+            if (a <= b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == ">=") {
+            if (a >= b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        }
+    }
+
+    void operatorCondReturnFalseTrue_DOUBLE(std::string operatorCond, double a, double b) {
+        if (operatorCond == "=") {
+            if (a == b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == "<") {
+            if (a < b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == ">") {
+            if (a > b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == "<=") {
+            if (a <= b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        } else if (operatorCond == ">=") {
+            if (a >= b) { std::cout << "vrai"; } else { std::cout << "faux"; }
+        }
+    }
+}
---
+++ src/Instructions/Print.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+namespace FPL::Instruction::Print {
+    void operatorCondReturnFalseTrue_INT(std::string operatorCond, int a, int b);
+    void operatorCondReturnFalseTrue_DOUBLE(std::string operatorCond, double a, double b);
+}
